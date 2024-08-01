5d1767c10c6c1604466b5917e64827404eb3bbdc
Mon <simon.juza@gmail.com>
2024-08-01 11:46:49 +0200

    start of remake

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MistsOfThelema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1241, 583);
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MistsOfThelema
+{
+    public static class player
+    {
+        public readonly static Keys Up = Keys.W;
+        public readonly static Keys Down = Keys.S;  
+        public readonly static Keys Left = Keys.A;
+        public readonly static Keys Right = Keys.D;
+
+        public readonly static Keys Interact = Keys.F;
+
+        public static bool IsUp = false;
+        public static bool IsDown = false;
+        public static bool IsLeft = false;
+        public static bool IsRight = false;
+
+        public static bool IsInteracting = false;
+    }
+}
